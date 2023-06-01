ed0c0fec4e436a74daad9a8e600bc564b10175b1
Jon Atkinson <jon@jonatkinson.co.uk>
2023-06-01 08:36:35 +0100

    feat: Initial version of gho.sh and README.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Bash Script for Opening GitHub Repository in a Web Browser - GHO
+
+The `gho` script (GitHub Opener) is a Bash utility that identifies if the current or any parent directory is a Git repository. If it is, the script extracts the GitHub URL of the repository from its `.git/config` file. It then attempts to open the repository in Safari, or in the web browser specified by the `$BROWSER` environment variable if Safari is unavailable.
+
+## Installation
+
+Clone the repository where the `gho` script is hosted:
+
+    git clone https://github.com/jonatkinson/gho && cd gho
+
+Finally, copy the gho script to /usr/local/bin to make it globally accessible, and then executable. This operation might require sudo.
+
+    sudo cp gho /usr/local/bin
+    sudo chmod + /usr/local/bin/gho
+
+## Usage
+
+    gho [-v] [command]
+
+### Options
+
+- -v or --verbose: Use this flag for the script to output informational messages as it runs. If it isn't set, the script operates quietly, only outputting error messages when they occur.
+
+### Commands
+
+- `pr`: If this command is supplied, the script opens the Pull Request page for the GitHub repository.
+- `i`: If this command is supplied, the script opens the Issues page for the GitHub repository.
+If no command is supplied, the script opens the main page for the GitHub repository.
+
+## Example Usage
+
+    gho -v pr
+
+In this example, the script will output informational messages as it runs and will open the Pull Request page for the GitHub repository in the web browser.
---
+++ gho.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Initialize flag variables
+verbose=0
+
+# Parse command-line options
+while getopts "v" option; do
+    case "${option}" in
+        v)
+            verbose=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Function to recursively check for .git directory in current and parent directories
+function check_git() {
+    local dir="$1"
+    while [[ "$dir" != "" && "$dir" != "$HOME" ]]; do
+        if [[ -d "${dir}/.git" ]]; then
+            [[ $verbose -eq 1 ]] && echo "Found .git repository at $dir"
+            echo "${dir}"
+            return 0
+        fi
+        dir="$(dirname "$dir")"
+    done
+    echo "This directory or any parent directories are not a .git repository" >&2
+    return 1
+}
+
+# Get the current directory
+current_dir="$(pwd)"
+
+# Check if current directory (or any parent directory) is a git repository
+git_dir=$(check_git "$current_dir")
+if [[ $? -eq 0 ]]; then
+    # Parse the Github URL from the .git/config
+    github_url=$(grep "url" "${git_dir}/.git/config" | head -n 1 | awk -F'=' '{ print $2 }' | xargs)
+    github_url=${github_url/ssh:\/\/git@github.com\//https:\/\/github.com\/}
+    github_url=${github_url/.git/}
+
+    [[ $verbose -eq 1 ]] && echo "Github URL is: $github_url"
+
+    # Append the relevant path if an argument was supplied
+    case "$1" in
+        pr)
+            github_url="${github_url}/pulls"
+            ;;
+        i)
+            github_url="${github_url}/issues"
+            ;;
+        "")
+            ;;
+        *)
+            echo "Invalid argument: $1" >&2
+            exit 1
+            ;;
+    esac
+
+    # Open the Github URL in Safari if it exists, otherwise use $BROWSER
+    if command -v safari >/dev/null 2>&1; then
+        open -a safari "$github_url"
+    elif [[ -n $BROWSER ]]; then
+        $BROWSER "$github_url"
+    else
+        echo "Unable to find a browser to open Github URL" >&2
+        exit 1
+    fi
+else
+    exit 1
+fi
